73ede14b92fed5535e1738b11845ae4281191842
Dongni Du <du000031@algonquinlive.com>
2023-01-29 09:50:41 -0500

    Finished week 2

--- app/src/main/java/algonquin/cst2335/du000031/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/du000031/ui/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import algonquin.cst2335.du000031.data.MainViewModel;
 import algonquin.cst2335.du000031.databinding.ActivityMainBinding;
@@ -23,8 +24,34 @@ public class MainActivity extends AppCompatActivity {
             binding.myText.setText("Your edit text has: " + s);
         });
 
+        model.isSelected.observe(this, selected -> {
+            binding.myCheckbox.setChecked(selected);
+            binding.mySwitch.setChecked(selected);
+            binding.myRadioButton.setChecked(selected);
+
+            Toast.makeText(this, "This value is now: " + selected, Toast.LENGTH_SHORT).show();
+        });
+
         binding.myButton.setOnClickListener(v -> {
             model.editString.postValue(binding.myEditText.getText().toString());
+        });
+
+        binding.myCheckbox.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+
+        binding.mySwitch.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+
+        binding.myRadioButton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isSelected.postValue(isChecked);
+        });
+
+        binding.myImageButton.setOnClickListener(v -> {
+            Toast.makeText(this,
+                    "The width = " + binding.myImageButton.getWidth() + " and height = " + binding.myImageButton.getHeight(),
+                    Toast.LENGTH_SHORT).show();
         });
     }
 }
